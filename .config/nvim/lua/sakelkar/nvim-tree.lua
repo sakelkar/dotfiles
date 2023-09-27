128f9558d5eb76bb708769c0c493c5e63898ce36
Sameer Kelkar <sakelkar@cisco.com>
2023-09-27 09:49:54 +0530

    fixed nvim-tree setup file;

--- .config/nvim/lua/sakelkar/nvim-tree.lua
+++ .config/nvim/lua/sakelkar/nvim-tree.lua
@@ -3,14 +3,15 @@ if not status_ok then
   return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-  return
-end
+--local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
+--if not config_status_ok then
+--  print("not found nvim-tree config")
+--  return
+--end
 
 local icons = require "sakelkar.icons"
 
-local tree_cb = nvim_tree_config.nvim_tree_callback
+--local tree_cb = nvim_tree_config.nvim_tree_callback
 
 --local utils = require "nvim-tree.utils"
 
@@ -38,11 +39,11 @@ nvim_tree.setup {
   -- disable_netrw = true,
   -- hijack_netrw = true,
   -- open_on_setup = false,
-  ignore_ft_on_setup = {
-    "startify",
-    "dashboard",
-    "alpha",
-  },
+  --ignore_ft_on_setup = {
+    --"startify",
+    --"dashboard",
+    --"alpha",
+  --},
   filters = {
     custom = { ".git" },
     exclude = { ".gitignore" },
@@ -148,14 +149,14 @@ nvim_tree.setup {
     hide_root_folder = false,
     side = "left",
     -- auto_resize = true,
-    mappings = {
-      custom_only = false,
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
-    },
+    --mappings = {
+      --custom_only = false,
+      --list = {
+        --{ key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+        --{ key = "h", cb = tree_cb "close_node" },
+        --{ key = "v", cb = tree_cb "vsplit" },
+      --},
+    --},
     number = false,
     relativenumber = false,
   },
